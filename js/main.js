902bbe3a4b9f42a00d8bb138e773ca660e10610f
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-04-14 10:01:52 -0500

    color changers selects form array of colors

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const flipBtn = document.querySelector("#flipBtn").addEventListener("click", btnClick)
 const colorDiv = document.querySelector("#flip")
+const colors = ["white", "green", "pruple", "orange", "blue", "aqua", "yellow", "brown", "red"]
 
 function btnClick(){
-    if(colorDiv.style.backgroundColor === "red"){
-        colorDiv.style.backgroundColor="white"
-    } else {
-        colorDiv.style.backgroundColor="red"
-    }
+    //gets a random number from tyhe getRandomNumber function and uses it to select color from colors array
+    const randomNumber = getRandomNumber();
+    colorDiv.style.backgroundColor = colors[randomNumber];
+}
+
+function getRandomNumber(){
+    //gets a random number based on the lenght of the colors array and rounds it to a whole number
+    return Math.floor(Math.random() * colors.length)
 }
